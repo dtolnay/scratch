86e44ce7f19b8869913e17064c4c17c4a3aa5e79
David Tolnay <dtolnay@gmail.com>
2020-09-16 23:04:52 -0400

    Write crate-level rustdoc

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,24 @@
+//! This crate exposes a compile-time temporary directory sharable by multiple
+//! crates in a build graph and erased by `cargo clean`.
+//!
+//! The intended usage is from a build.rs Cargo build script:
+//!
+//! ```toml
+//! # Cargo.toml
+//!
+//! [build-dependencies]
+//! scratch = "0.0"
+//! ```
+//!
+//! ```
+//! // build.rs
+//!
+//! fn main() {
+//!     let dir = scratch::path("mycrate");
+//!     // ... write or read inside of that path
+//! }
+//! ```
+
 use std::path::{Path, PathBuf};
 
 pub fn path(suffix: &str) -> PathBuf {
